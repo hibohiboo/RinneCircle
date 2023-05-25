3482686f3d6026ecd8d5809f9bd3396075553917
hibohiboo <swdice66@gmail.com>
2023-05-26 01:00:47 +0900

    add 画面遷移のテスト

--- packages/ui_components/src/components/templates/Posts/PostItem/index.test.tsx
+++ packages/ui_components/src/components/templates/Posts/PostItem/index.test.tsx
@@ -1,9 +1,12 @@
 import { getPostsData } from "@/services/server/Posts/__mock__/fixture";
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-// import mockRouter from "next-router-mock";
 import { PostItem } from "./";
-import { MemoryRouter } from "react-router-dom";
+import {
+  MemoryRouter,
+  RouterProvider,
+  createMemoryRouter,
+} from "react-router-dom";
 
 const user = userEvent.setup();
 
@@ -24,8 +27,27 @@ test("リンクのアクセシブルネームはタイトルを参照する", as
   expect(link).toHaveAccessibleName(post.title);
 });
 
-// test("リンクを押下すると画面遷移する", async () => {
-//   const { post, click } = setup();
-//   await click();
-//   expect(mockRouter).toMatchObject({ pathname: `/posts/${post.id}` });
-// });
+test("リンクを押下すると画面遷移する", async () => {
+  const post = getPostsData.posts[0];
+
+  const router = createMemoryRouter(
+    [
+      {
+        path: "/posts",
+        element: <PostItem post={{ ...post, published: true }} />,
+      },
+      {
+        path: "/posts/1",
+        element: <></>,
+      },
+    ],
+    { initialEntries: ["/posts"] },
+  );
+  render(<RouterProvider router={router} />);
+  const link = screen.getByRole("link");
+  const click = () => user.click(link);
+  await click();
+  await waitFor(() => {
+    expect(router.state.location.pathname).toEqual(`/posts/${post.id}`);
+  });
+});
