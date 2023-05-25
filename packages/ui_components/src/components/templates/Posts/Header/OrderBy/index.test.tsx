fc4d0d34ad39bbbddcde9e5b9621287cd604fa80
hibohiboo <swdice66@gmail.com>
2023-05-26 01:12:10 +0900

    fix test

--- packages/ui_components/src/components/templates/Posts/Header/OrderBy/index.test.tsx
+++ packages/ui_components/src/components/templates/Posts/Header/OrderBy/index.test.tsx
@@ -1,9 +1,13 @@
-import { render, screen } from "@testing-library/react";
-// import userEvent from "@testing-library/user-event";
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { OrderBy } from "./";
-import { MemoryRouter } from "react-router-dom";
+import {
+  MemoryRouter,
+  RouterProvider,
+  createMemoryRouter,
+} from "react-router-dom";
 
-// const user = userEvent.setup();
+const user = userEvent.setup();
 
 function setup(asPath = "/posts") {
   render(
@@ -20,12 +24,22 @@ test("初期表示、query.orderBy なしの場合「公開日時順」が選択
   expect(combobox).toHaveDisplayValue("更新日時順");
 });
 
-// test("選択した場合、query.orderBy が設定される", async () => {
-//   const { combobox } = setup();
-//   await user.selectOptions(combobox, "starCount");
-//   expect(combobox).toHaveDisplayValue("スター数順");
-//   expect(mockRouter).toMatchObject({
-//     pathname: "/posts",
-//     query: { orderBy: "starCount" },
-//   });
-// });
+test("選択した場合、query.orderBy が設定される", async () => {
+  const router = createMemoryRouter(
+    [
+      {
+        path: "/posts",
+        element: <OrderBy />,
+      },
+    ],
+    { initialEntries: ["/posts"] },
+  );
+  render(<RouterProvider router={router} />);
+  const combobox = screen.getByRole("combobox");
+  await user.selectOptions(combobox, "starCount");
+  expect(combobox).toHaveDisplayValue("スター数順");
+  await waitFor(() => {
+    expect(router.state.location.pathname).toBe(`/posts`);
+    expect(router.state.location.search).toBe(`?orderBy=starCount`);
+  });
+});
