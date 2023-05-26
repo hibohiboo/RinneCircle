5cd4043de2c91099da0ab86ed47fcdbd0d438559
hibohiboo <swdice66@gmail.com>
2023-05-26 22:19:16 +0900

    fix astro v1 to v2

--- docs/astro/astro.config.mjs
+++ docs/astro/astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: `http://astro.build`,
+  site: `https://hibohiboo.github.io/`,
   base: "RinneCircle",
 });
--- docs/astro/src/pages/ja/history.md
+++ docs/astro/src/pages/ja/history.md
@@ -6,5 +6,6 @@ layout: ../../layouts/MainLayout.astro
 
 **更新履歴**
 
-2023.05.25 Storybook に msw 導入
+2023.05.26 astro v1 to v2  
+2023.05.25 Storybook に msw 導入  
 2023.05.25 posts 追加
