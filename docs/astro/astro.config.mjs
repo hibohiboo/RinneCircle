98adf46a8038d5fee9e464e5be40b7209a0f6c36
hibohiboo <swdice66@gmail.com>
2023-05-26 22:30:32 +0900

    fix v2 AB#385

--- docs/astro/astro.config.mjs
+++ docs/astro/astro.config.mjs
@@ -1,7 +1,4 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  site: `https://hibohiboo.github.io/`,
-  base: "RinneCircle",
-});
+export default defineConfig({});
