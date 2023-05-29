bfb6cee3707a83552586b0e85e0d05e9e2775f61
hibohiboo <swdice66@gmail.com>
2023-05-29 22:53:52 +0900

    fix build AB#391

--- apps/front/src/main.tsx
+++ apps/front/src/main.tsx
@@ -1,15 +1,27 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
 declare let VITE_DEFINE_BASE_PATH: string;
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+    },
+  ],
+  {
+    basename: `/${VITE_DEFINE_BASE_PATH}`,
+  },
+);
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <BrowserRouter basename={VITE_DEFINE_BASE_PATH}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>,
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 );
