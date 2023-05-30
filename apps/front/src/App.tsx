8e58c4f539501ce75eb7454b7e4f47d036961f6a
hibohiboo <swdice66@gmail.com>
2023-05-31 00:40:24 +0900

    fix ログイン確認（失敗） AB#394

--- apps/front/src/App.tsx
+++ apps/front/src/App.tsx
@@ -1,6 +1,30 @@
 import { RinneBuilderTop } from "@rinne-circle/components";
-
+import { useEffect } from "react";
+import { Auth } from "@aws-amplify/auth";
+Auth.configure({
+  region: import.meta.env.VITE_APP_AUTH_REGION,
+  userPoolId: import.meta.env.VITE_APP_AUTH_USER_POOL_ID,
+  userPoolWebClientId: import.meta.env.VITE_APP_AUTH_USER_POOL_WEB_CLIENT_ID,
+  authenticationFlowType: "USER_SRP_AUTH",
+});
 function App() {
+  useEffect(() => {
+    const init = async () => {
+      try {
+        const result = await Auth.currentAuthenticatedUser();
+        console.log("auth", result);
+      } catch (e) {
+        console.log(e);
+        try {
+          const credentials = await Auth.currentCredentials();
+          console.log("credentials", credentials);
+        } catch (e) {
+          console.log("credential error", e);
+        }
+      }
+    };
+    void init();
+  }, []);
   return (
     <div
       style={{
