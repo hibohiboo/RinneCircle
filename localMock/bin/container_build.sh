6f16a4a48dfce2b72c41a1fe90076894e26d48bf
hibohiboo <swdice66@gmail.com>
2023-06-10 23:37:42 +0900

    fix package.json AB#396

--- localMock/bin/container_build.sh
+++ localMock/bin/container_build.sh
@@ -3,5 +3,12 @@
 # このシェルスクリプトのディレクトリの絶対パスを取得。
 bin_dir=$(cd $(dirname $0) && pwd)
 parent_dir=$bin_dir/..
+docker_dir=$parent_dir/docker
+
+cp $parent_dir/package.json $docker_dir/apollo/package.json
+cp $parent_dir/package-lock.json $docker_dir/apollo/package-lock.json
+cp $parent_dir/tsconfig.json $docker_dir/apollo/tsconfig.json
+
+
 # cp $parent_dir/package.json $parent_dir/docker/node/package.json
-cd $parent_dir/docker && docker-compose build
+cd $docker_dir && docker-compose build
