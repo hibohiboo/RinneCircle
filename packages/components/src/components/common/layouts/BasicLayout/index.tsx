913f54f54829fc90f29203b3423d4725444c7623
hibohiboo <swdice66@gmail.com>
2023-06-15 01:28:14 +0900

    add footer AB#397

--- packages/components/src/components/common/layouts/BasicLayout/index.tsx
+++ packages/components/src/components/common/layouts/BasicLayout/index.tsx
@@ -1,11 +1,16 @@
 import { ReactElement } from "react";
 
 import styles from "./styles.module.css";
+import { Footer } from "./Footer";
 
 export const BasicLayout = (page: ReactElement) => {
   return (
-    <div className={styles.root}>
-      <main>{page}</main>
-    </div>
+    <>
+      <div className={styles.root}>
+        <main>{page}</main>
+      </div>
+
+      <Footer />
+    </>
   );
 };
