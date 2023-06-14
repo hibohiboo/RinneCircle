f6d22cc90176b9cbdd39fa6a76eb8acabd9138ce
hibohiboo <swdice66@gmail.com>
2023-06-14 21:04:21 +0900

    fix build green AB#396

--- apps/front/vite.config.ts
+++ apps/front/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import dotenv from "dotenv";
+import path from "path";
 
 dotenv.config({ path: "./.env.local" });
 // https://vitejs.dev/config/
@@ -9,5 +10,13 @@ export default defineConfig({
   base: `/${process.env.SUB_DIR_PATH_BUILDER}/`,
   define: {
     VITE_DEFINE_BASE_PATH: JSON.stringify(process.env.SUB_DIR_PATH_BUILDER),
+  },
+  build: {
+    rollupOptions: {},
+  },
+  resolve: {
+    alias: {
+      "@": path.join(__dirname, "../../packages/components/src"),
+    },
   },
 });
