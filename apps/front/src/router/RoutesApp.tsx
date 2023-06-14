7a0d38e91a74f93aca3afe81c2ff619c0bd7e00e
hibohiboo <swdice66@gmail.com>
2023-06-15 01:09:15 +0900

    chore update package AB#397

--- apps/front/src/router/RoutesApp.tsx
+++ apps/front/src/router/RoutesApp.tsx
@@ -2,5 +2,7 @@ import { RouterProvider } from "react-router-dom";
 import { router } from ".";
 
 export function RoutesApp() {
-  return <RouterProvider router={router} />;
+  return (
+    <RouterProvider router={router} future={{ v7_startTransition: true }} />
+  );
 }
